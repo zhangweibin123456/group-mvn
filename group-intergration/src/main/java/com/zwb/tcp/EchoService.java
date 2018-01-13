2a96cbeb23a4fbb61524db0de7a188c8b7b6891f
my <my@192.168.1.5>
2018-01-13 14:27:31 +0800

    1

---
+++ group-intergration/src/main/java/com/zwb/tcp/EchoService.java
@@ -0,0 +1,12 @@
+package com.zwb.tcp;
+
+public class EchoService {
+
+	public String test(String input) {
+		if ("FAIL".equals(input)) {
+			throw new RuntimeException("Failure Demonstration");
+		}
+		return "echo:" + input;
+	}
+
+}
---
+++ group-intergration/src/main/java/com/zwb/tcp/SimpleGateway.java
@@ -0,0 +1,10 @@
+package com.zwb.tcp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface SimpleGateway {
+
+	public String send(String text);
+
+}
