55efcea87e1f03722d114df15642d8054e389a2c
my <my@192.168.1.5>
2018-01-13 15:29:15 +0800

    1

---
+++ group-data/src/test/java/com/zwb/elasticsearch/elasticsearchTest.java
@@ -0,0 +1,21 @@
+package com.zwb.elasticsearch;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:elasticsearch/elasticsearch.xml" })
+public class elasticsearchTest {
+
+	
+	@Test
+	public void Test() {
+
+	}
+
+	
+}
