ef753b55149426426f7162481d9b99b08e664a04
my <my@192.168.1.5>
2018-01-13 14:21:05 +0800

    1

---
+++ group-intergration/src/main/java/com/zwb/file/FileGateway.java
@@ -0,0 +1,14 @@
+package com.zwb.file;
+
+import java.io.File;
+import java.util.Map;
+
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface FileGateway {
+
+	public File send(@Headers Map<String, Object> map,File file);
+	
+}
