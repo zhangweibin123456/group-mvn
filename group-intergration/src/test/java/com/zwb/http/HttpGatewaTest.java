03e78d58aa7bf5f81e90bea5608edb87716456e1
my <my@192.168.1.5>
2018-01-13 14:09:10 +0800

    1

---
+++ group-intergration/src/test/java/com/zwb/http/HttpGatewaTest.java
@@ -0,0 +1,34 @@
+package com.zwb.http;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:http/httpGateway.xml" })
+public class HttpGatewaTest {
+
+	@Autowired
+	private RequestGateway requestGateway;
+
+	@Test
+	public void test() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("urlPath", "https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=15288888888");
+		map.put("httpMethod", "GET");
+		map.put("responseType", "java.lang.String");
+		String response = requestGateway.echo(map, "456");
+		System.out.println("什么玩意：" + response);
+		// map.put("urlPath", "http://localhost:8080/sssDome/testHttp/getFile");
+		// map.put("httpMethod", "POST");
+		// map.put("responseType", "java.io.File");
+		// //String response= requestGateway.echo(map,"456");
+		// File file= requestGateway.getFile(map, "456");
+		// System.out.println(file);
+	}
+
+}
