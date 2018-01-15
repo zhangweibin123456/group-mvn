45653af951397e28e4e3fc0382e92a60bd54c6a9
zhangweibin <zhangweibin@DESKTOP-KJI47G7>
2018-01-15 16:15:46 +0800

    jms mq

---
+++ group-intergration/src/main/java/com/zwb/jms/JmsGateway.java
@@ -0,0 +1,12 @@
+package com.zwb.jms;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface JmsGateway {
+
+	public String transport(String payload);//有返回值
+	
+	public void sead(String payload);//无返回值
+}
